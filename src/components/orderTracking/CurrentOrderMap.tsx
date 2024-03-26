da93ba7e0b0f2f6729bfc36198972eccdb28d34f
Dev-SalamSheikh <dev.salamsheikh@gmail.com>
2024-03-27 05:32:32 +0600

    current order map skin changed as well

--- src/components/orderTracking/CurrentOrderMap.tsx
+++ src/components/orderTracking/CurrentOrderMap.tsx
@@ -71,7 +71,7 @@ const CurrentOrderMap = () => {
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
+          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         const convertedGeoCode: [number, number] = [geoCode[0], geoCode[1]];
         <Polyline
