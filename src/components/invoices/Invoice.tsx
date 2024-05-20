f1ab041d5e66cb5dec187a54b228ad2e7dad8b79
deliverybotNYC <webmaster@deliverybotnyc.com>
2024-05-19 22:56:07 -0400

    invoioce

--- src/components/invoices/Invoice.tsx
+++ src/components/invoices/Invoice.tsx
@@ -1,24 +1,45 @@
 import ContentBox from "../reusable/ContentBox";
-
+import { Link } from "react-router-dom";
 import DownloadIcon from "../../assets/download-icon.svg";
 import CloseIcon from "../../assets/close-red.svg";
 import InvoiceImage from "../../assets/invoice.svg";
+import { useConfig, url } from "../../hooks/useConfig";
+import moment from "moment";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import UseGetOrderId from "../../hooks/UseGetOrderId";
 
 const Invoice = () => {
+  const InvoiceId = UseGetOrderId();
+  console.log(InvoiceId);
+  const config = useConfig();
+  // Get invoice data
+  const { isLoading, data, error } = useQuery({
+    queryKey: ["cat"],
+    queryFn: () => {
+      return axios.get(url + "/invoices", config).then((res) => res.data);
+    },
+  });
   return (
     <ContentBox>
       <div className="w-full h-full bg-white rounded-2xl p-5">
         {/* Header */}
         <div className="w-full flex items-center justify-end gap-5">
+          <p className="text-lg text-themeOrange">
+            {invoice?.invoice_id?.slice(0, 8)}
+            <span className="text-black font-semibold">
+              {invoice?.invoice_id?.slice(9)}
+            </span>
+          </p>
           {/* Download Icon */}
           <div>
             <img src={DownloadIcon} alt="download-icon" />
           </div>
 
           {/* Close Icon */}
-          <div>
+          <Link to="/invoices">
             <img src={CloseIcon} alt="download-icon" />
-          </div>
+          </Link>
         </div>
 
         {/* Invoice Image */}
